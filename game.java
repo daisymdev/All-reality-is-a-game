c6e9c1a4987fec711f036aa5aedc37fb6da95c76
Daisy Murillo <daisy21murillo@gmail.com>
2021-02-11 21:19:50 -0700

    this class stores a level object

--- game.java
+++ game.java
@@ -1,4 +1,32 @@
 // Daisy Murillo Javascript practice with DOM -- Feb 11, 2021 
 
+// let simpleLevelPlan = `
+// ......................
+// ..#................#..
+// ..#..............=.#..
+// ..#.........o.o....#..
+// ..#.@......#####...#..
+// ..#####............#..
+// ......#++++++++++++#..
+// ......##############..
+// ......................`;
 
 
+class Level {
+    constructor(plan) {
+        let rows = plan.trim().split("\n").map(l => [...l]);
+        this.height = rows.length;
+        this.width = rows[0].length;
+        this.startActors = [];
+
+        this.rows = rows.map((row, y) => {
+            return row.map((ch, x) => {
+                let type = levelChars[ch];
+                if (typeof type == "string") return type;
+                this.startActors.push(
+                    type.create(new Vec(x, y). ch));
+                return "empty";
+            });
+        });
+    }
+}

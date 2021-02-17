2fa5a2149fe9e2ae3885d65d966892711dd7d8f3
Daisy Murillo <daisy21murillo@gmail.com>
2021-02-16 22:26:58 -0700

    added Vec class that will be used for 2d values, such as the position and size of actors

--- game.js
+++ game.js
@@ -47,3 +47,22 @@ class State {
     }
 }
 
+class Vec {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    plus(other) {
+        return new Vec(this.x + other.x, this.y + other.y);
+    }
+
+    times(factor) {
+        return new Vec(this.x * factor, this.y * factor);
+    }
+}
+
+
+
+
+
